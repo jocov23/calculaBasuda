12b593d4340e5c18ce17cb959706d0d79c862e5a
jocov <jocov2309@gmail.com>
2026-03-01 20:05:16 -0300

    create: primeiro commit e criação do projeto função 'main' com conversão decimal para binaria apenas, string vector inicializado com funcao matematica que divide o decimal pela base continuadamente e armazena os restos fazendo typecasting int->string com func to_string(var)

---
+++ teste.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+std::vector<std::string> funcaoVetor (unsigned long long int decimal){
+    
+    std::vector<std::string> vetorConvertido = {"null"};
+    unsigned long long int resto;
+    //divisão inicial fora de loop para substituir valor de definição do vetorConvertido
+    resto = decimal % 2; 
+    std::string valorBinario = std::to_string(resto);
+    vetorConvertido[0] = valorBinario ;
+    std::cout << decimal << std::endl;
+    decimal/=2;
+    
+    //enquanto decimal for maior que 0, será constantemente dividido
+    //seu resto continuadamente guardado no vetorConvertido
+    while (decimal > 0){
+            
+            resto = decimal % 2;  
+            valorBinario = std::to_string(resto);
+            vetorConvertido.push_back(valorBinario);
+            std::cout << decimal << std::endl;
+            decimal/=2;
+
+    }
+    return vetorConvertido;
+}
+
+int main() {
+    //numero maximo = 18446744073709551615 (18 quintilhões)
+    unsigned long long int decimal = 1;
+    std::vector <std::string> vetorConvertido = funcaoVetor (decimal);
+
+    //iteração sobre os binarios convertidos
+    std::cout << "binario comeca aqui" << std::endl;
+    for (int i = 0; i < vetorConvertido.size(); i++){
+        std::cout << vetorConvertido[i]<< std::endl;
+    }
+    return 2311;
+    
+
+}
