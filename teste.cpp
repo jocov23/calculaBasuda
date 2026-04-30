c35eadb71c8c49273228745e0bba38a6cf9a122b
jocov <jocov2309@gmail.com>
2026-04-30 15:29:35 -0300

    update

--- teste.cpp
+++ teste.cpp
@@ -5,6 +5,7 @@
 int baseOrigem, baseDestino;
 
 std::map<std::string, std::string> valores = {
+    {"0", "0"},
     {"1", "1"},
     {"2", "2"},
     {"3", "3"},
@@ -19,7 +20,25 @@ std::map<std::string, std::string> valores = {
     {"12", "C"},
     {"13", "D"},
     {"14", "E"},
-    {"15", "F"}
+    {"15", "F"},
+    {"16", "G"},
+    {"16", "H"},
+    {"17", "I"},    
+    {"18", "J"},
+    {"19", "K"},
+    {"20", "L"},
+    {"21", "M"},
+    {"22", "N"},
+    {"23", "O"},
+    {"24", "P"},
+    {"25", "Q"},
+    {"26", "R"},
+    {"27", "S"},
+    {"28", "T"},
+    {"29", "U"},
+    {"30", "V"},
+    {"31", "W"},
+    {"32", "X"}
     
 };
 std::vector<std::string> funcaoVetor (unsigned long long int decimal){
@@ -49,14 +68,15 @@ std::vector<std::string> funcaoVetor (unsigned long long int decimal){
 
 int main() {
     //numero maximo = 18446744073709551615 (18 quintilhões)
-    unsigned long long int decimal = 184467440726178461;
+    unsigned long long int decimal = 18293449;
     std::cout << ("Digite a base de destino: ");
     std::cin >> (baseDestino);
     std::vector <std::string> vetorConvertido = funcaoVetor (decimal);
 
     //iteração sobre os binarios convertidos
     std::cout << "valor convertido comeca aqui" << std::endl;
-    for (int i = 0; i < vetorConvertido.size(); i++){
+    
+    for (int i = vetorConvertido.size()-1; i >= 0; i--){
         std::cout << vetorConvertido[i];
     }
     return 2311;
