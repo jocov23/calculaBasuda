64b6e0e91bf3518c9d784a5b321bba879c201e16
jocov <jocov2309@gmail.com>
2026-04-30 15:17:23 -0300

    update

--- teste.cpp
+++ teste.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
+int baseOrigem, baseDestino;
 
-
+std::map<std::string, std::string> valores = {
+    {"1", "1"},
+    {"2", "2"},
+    {"3", "3"},
+    {"4", "4"},
+    {"5", "5"},
+    {"6", "6"},
+    {"7", "7"},
+    {"8", "8"},
+    {"9", "9"},
+    {"10", "A"},
+    {"11", "B"},
+    {"12", "C"},
+    {"13", "D"},
+    {"14", "E"},
+    {"15", "F"}
+    
+};
 std::vector<std::string> funcaoVetor (unsigned long long int decimal){
     
     std::vector<std::string> vetorConvertido = {"null"};
     unsigned long long int resto;
     //divisão inicial fora de loop para substituir valor de definição do vetorConvertido
-    resto = decimal % 2; 
-    std::string valorBinario = std::to_string(resto);
-    vetorConvertido[0] = valorBinario ;
+    resto = decimal % baseDestino; 
+    std::string valorConvertidoString = std::to_string(resto);
+    vetorConvertido[0] = valores[valorConvertidoString] ;
     std::cout << decimal << std::endl;
-    decimal/=2;
+    decimal/=baseDestino;
     
     //enquanto decimal for maior que 0, será constantemente dividido
     //seu resto continuadamente guardado no vetorConvertido
     while (decimal > 0){
             
-            resto = decimal % 2;  
-            valorBinario = std::to_string(resto);
-            vetorConvertido.push_back(valorBinario);
+            resto = decimal % baseDestino;  
+            valorConvertidoString = std::to_string(resto);
+            vetorConvertido.push_back(valores[valorConvertidoString]);
             std::cout << decimal << std::endl;
-            decimal/=2;
+            decimal/=baseDestino;
 
     }
     return vetorConvertido;
@@ -31,12 +50,14 @@ std::vector<std::string> funcaoVetor (unsigned long long int decimal){
 int main() {
     //numero maximo = 18446744073709551615 (18 quintilhões)
     unsigned long long int decimal = 184467440726178461;
+    std::cout << ("Digite a base de destino: ");
+    std::cin >> (baseDestino);
     std::vector <std::string> vetorConvertido = funcaoVetor (decimal);
 
     //iteração sobre os binarios convertidos
-    std::cout << "binario comeca aqui" << std::endl;
+    std::cout << "valor convertido comeca aqui" << std::endl;
     for (int i = 0; i < vetorConvertido.size(); i++){
-        std::cout << vetorConvertido[i]<< std::endl;
+        std::cout << vetorConvertido[i];
     }
     return 2311;
     
